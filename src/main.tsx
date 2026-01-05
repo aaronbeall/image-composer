c17f5f48dd19b62c9665f0666e47baecc2760eeb
Aaron Beall <contact@abeall.com>
2026-01-05 16:07:12 -0500

    refactor: replace App with AppNewLayout component and rename to App component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppNewLayout from './AppNewLayout.tsx'
+import App from './App.tsx'
 import { ThemeProvider } from './components/ui/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
       enableSystem
       disableTransitionOnChange
     >
-      <AppNewLayout />
+      <App />
     </ThemeProvider>
   </StrictMode>,
 )
